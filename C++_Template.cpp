7733e0f6a40c66f177b68ff2dbf62a69901f72e6
luismo <luismoax@gmail.com>
2018-10-26 15:37:52 -0400

    Inicio

---
+++ C++_Template.cpp
@@ -0,0 +1,49 @@
+/*
+	Author: Luis Manuel Díaz Barón (LUISMO)
+	Problem:
+	Online Judge:
+	Idea:
+*/
+#include<bits/stdc++.h>
+// Types
+#define ll long long
+#define ull unsigned long long
+// IO
+#define sf scanf
+#define pf printf
+#define endl "\n"
+
+using namespace std;
+
+typedef pair<int, int> pii;
+const ll inf = 1e16 + 3;
+const int mod = 1e9 + 7;
+const int lim = 2e5 + 2;
+
+
+void solve()
+{
+
+}
+
+void fastIO()
+{
+	cin.sync_with_stdio(false);
+	cin.tie(0);
+}
+
+int main()
+{
+	if(fopen("d:\\lmo.in","r") != NULL)
+	{
+		freopen("d:\\lmo.in","r",stdin);
+	}
+	else if(fopen("/media/luismo/Pablo's Destruction/lmo.in","r") != NULL)
+	{
+		freopen("/media/luismo/Pablo's Destruction/lmo.in", "r", stdin);
+	}
+
+	fastIO();
+
+	solve();
+}
